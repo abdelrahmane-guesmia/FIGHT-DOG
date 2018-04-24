1e72cb3e62d27b165f6b84172e11602b5df54c8f
enzo <enzo.gislard@viacesi.fr>
2018-04-24 12:59:12 +0200

    mobile non finis

--- jpu2016/dogfight/model/Mobile.java
+++ jpu2016/dogfight/model/Mobile.java
@@ -1,5 +1,55 @@
 package jpu2016.dogfight.model;
 
+import java.awt.Point;
+
 public class Mobile {
 
+	private int speed;
+	
+	public Mobile(Direction direction, Position position, Dimension dimension, int speed, String image){
+		
+	}
+	public Direction getDirection(){
+		return null;
+	}
+	
+	public void setDirection(Direction direction){		
+	}
+	
+	public Point getPosition(){
+		return null;
+	}
+	
+	public Dimension getDimension(){
+		return null;
+	}
+	public int getSpeed(){
+		return 0;	
+	}
+	
+	public int getWidth(){
+		return 0;
+	}
+	 public int getHeight(){
+		 return 0;
+	}
+	 
+	public void move() {
+	}
+	
+	public void placeInArea(Iarea area){
+	}
+	
+	public boolean isPlayer(int player){
+		return false;
+	}
+	
+	private void moveUp(){
+	}
+	
+	
+	
+	
+	
+	
 }

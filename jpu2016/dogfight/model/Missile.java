a64a63767f868141fe2e8f263d696f16acf3ecb6
AntoineCasenove <33152572+AntoineCasenove@users.noreply.github.com>
2018-04-24 12:00:01 +0200

    Missile 2.0

--- jpu2016/dogfight/model/Missile.java
+++ jpu2016/dogfight/model/Missile.java
@@ -1,22 +1,22 @@
 package jpu2016.dogfight.model;
 
 public class Missile {
-	private int SPEED = 4;
-	private int WIDTH = 30;
-	private int HEIGHT = 10;
-	private int MAX_DISTANCE_TRAVELED = 1400;
-	private String IMAGE = "missile";
+	private static final int SPEED = 4;
+	private static final int WIDTH = 30;
+	private static final int HEIGHT = 10;
+	private static final int MAX_DISTANCE_TRAVELED = 1400;
+	private static final String IMAGE = "missile";
 	private int distanceTraveled = 0;
 	
 	public Missile(Direction direction, Dimension dimension){
 		//Completer
 	}
 	
-	public int getWidthADirection(Direction direction){
+	public static int getWidthADirection(Direction direction){
 		return 0;
 	}
 	
-	public int getHeightWithADirection(Direction direction){
+	public static int getHeightWithADirection(Direction direction){
 		return 0;
 	}
 	

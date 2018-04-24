a0638096b52759fd333a1a5007661a9e540838d3
AntoineCasenove <33152572+AntoineCasenove@users.noreply.github.com>
2018-04-24 11:27:17 +0200

    DogfightModel à compléter

--- jpu2016/dogfight/model/DogfightModel.java
+++ jpu2016/dogfight/model/DogfightModel.java
@@ -1,5 +1,40 @@
 package jpu2016.dogfight.model;
 
-public class DogfightModel {
+import java.util.ArrayList;
 
+public class DogfightModel {
+	
+	public DogfightModel(){
+	}
+	
+	public IArea getArea(){
+		return null;
+		//A completer
+	}
+	
+	public void buildArea(Dimension dimension){
+		//A completer
+	}
+	
+	public void addMobile(IMobile Mobile){
+		//A completer
+	}
+	
+	public void removeMobile(IMobile Mobile){
+		//A completer
+	}
+	
+	public ArrayList<IMobile> getMobiles(){
+		return null;
+		//A completer
+	}
+	
+	public IMobile getMobilesByPlayer(int player){
+		return null;
+		//A completer
+	}
+	
+	public void setMobilesHavesMoved(){
+		//A completer
+	}
 }

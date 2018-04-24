95d77cc80e04f6705d9e1292ec1623bc8722f3ab
AntoineCasenove <33152572+AntoineCasenove@users.noreply.github.com>
2018-04-24 10:19:28 +0200

    Salam.

--- src/salam.java
+++ src/salam.java
@@ -10,11 +10,9 @@ public class salam {
 		System.out.println ("enzo est bite");
 		for(i=0;i<100;i++){
 			for(j=0;j<100;j++){
-			System.out.print("Oui Enzo je t'aime.  ");
-			System.out.print("Ta mere antoine et ugo t'es moche");
+			System.out.print("Oui Enzo.  ");
 			}
 			System.out.print("\n");
 		}
 	}
-	
 }

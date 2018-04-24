777917453d731103c79f09693e7dfa5763d9eb84
AntoineCasenove <33152572+AntoineCasenove@users.noreply.github.com>
2018-04-24 10:14:33 +0200

    Oui Enzo.

--- src/salam.java
+++ src/salam.java
@@ -1,10 +1,19 @@
 package src;
 
 public class salam {
-
+	
 	public static void main(String[] args) {
+		
+		int i, j;
+		
 		System.out.println ("enzo est gentil");
 		System.out.println ("enzo est bite");
+		for(i=0;i<100;i++){
+			for(j=0;j<100;j++){
+			System.out.print("Oui Enzo.  ");
+			}
+			System.out.print("\n");
+		}
 	}
-
+	
 }

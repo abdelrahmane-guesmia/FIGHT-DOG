aca659c9b827a5d7eb46bbc9ef234558a26aeba2
yacine-bedrani <yacine.bedrani>
2018-04-24 11:47:57 +0200

    yo

--- jpu2016/dogfight/model/Missile.java
+++ jpu2016/dogfight/model/Missile.java
@@ -1,5 +1,31 @@
 package jpu2016.dogfight.model;
 
 public class Missile {
+	private int SPEED = 4;
+	private int WIDTH = 30;
+	private int HEIGHT = 10;
+	private int MAX_DISTANCE_TRAVELED = 1400;
+	private String IMAGE = "missile";
+	private int distanceTraveled = 0;
+	
+	public Missile(Direction direction, Dimension dimension){
+		//A completer 
+	}
+	
+	public int getWidthADirection(Direction direction){
+		return 0;
+	}
+	
+	public int getHeightWithADirection(Direction direction){
+		return 0;
+	}
+	
+	public void move(){
+		return;
+	}
+	
+	public boolean isWeapon(){
+		return false;
+	}
 
 }

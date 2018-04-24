c0350ceadd5646c06607bf0227366cc3e4aad15d
AntoineCasenove <33152572+AntoineCasenove@users.noreply.github.com>
2018-04-24 11:42:23 +0200

    Classe Dimension

--- jpu2016/dogfight/model/Dimension.java
+++ jpu2016/dogfight/model/Dimension.java
@@ -1,5 +1,30 @@
 package jpu2016.dogfight.model;
 
 public class Dimension {
+	private int width;
+	private int height;
+	
+	public Dimension(int width, int height){
+		//A completer
+	}
+	
+	public Dimension(Dimension dimension){
+		//A completer
+	}
 
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
 }

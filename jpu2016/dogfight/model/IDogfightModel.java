25f1ed20d1145e726814fbdc0218ff402f860b39
AntoineCasenove <33152572+AntoineCasenove@users.noreply.github.com>
2018-04-24 11:34:55 +0200

    Nouvelles classes

--- jpu2016/dogfight/model/IDogfightModel.java
+++ jpu2016/dogfight/model/IDogfightModel.java
@@ -1,5 +1,36 @@
 package jpu2016.dogfight.model;
 
-public class IDogfightModel {
+import java.util.ArrayList;
 
+public class IDogfightModel {
+	public IArea getArea(){
+		return null;
+		//A completer
+	}
+	
+	public void buildArea(Dimension dimension){
+		//A completer
+	}
+	
+	public void addMobile(IMobile Mobile){
+		//A completer
+	}
+	
+	public void removeMobile(IMobile Mobile){
+		//A completer
+	}
+	
+	public ArrayList<IMobile> getMobiles(){
+		return null;
+		//A completer
+	}
+	
+	public IMobile getMobileByPlayer(int player){
+		return null;
+		//A completer
+	}
+	
+	public void setMobilesHavesMoved(){
+		//A completer
+	}
 }
--- jpu2016/dogfight/model/Sky.java
+++ jpu2016/dogfight/model/Sky.java
@@ -1,5 +1,16 @@
 package jpu2016.dogfight.model;
 
 public class Sky {
-
+	public Sky(){
+	}
+	
+	public Dimension getDimension(){
+		return null;
+		//A completer
+	}
+	
+	public Image getImage(){
+		return null;
+		//A completer
+	}
 }
